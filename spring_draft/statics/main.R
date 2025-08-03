74a9645973a0780b9bd307d004b48d56348d61b5
becchi <mebiusu1968@gmail.com>
2025-08-04 02:07:03 +0900

    分析終了

--- spring_draft/statics/main.R
+++ spring_draft/statics/main.R
@@ -32,8 +32,7 @@ tidy_model_2$model <- "2:科学技術イノベーション政策\n(Q8.19)"
 
 combined_models_filtered <- rbind(tidy_model_1, tidy_model_2)
 
-# 1. labs()の中で、タイトルとサブタイトルに改行（\n）を入れる
-#    これにより、テキストが縦に長くなり、横幅に余裕が生まれる
+# 係数プロットを作成する
 revised_plot <- ggplot(combined_models_filtered, aes(x = estimate, y = model, color = model)) +
     geom_point(size = 4) +
     geom_errorbarh(aes(xmin = conf.low, xmax = conf.high), height = 0.2) +
@@ -49,8 +48,44 @@ revised_plot <- ggplot(combined_models_filtered, aes(x = estimate, y = model, co
 
 # 2. ggsave()で保存する際の横幅（width）を少し広げる
 #    こうすることで、タイトル全体が切れることなく収まる
-ggsave("my_coefficient_plot_revised.png", 
+ggsave("my_coefficient_plot_revised.png",
     plot = revised_plot, 
     width = 10,  # 以前より大きくする
     height = 6,   # 以前より大きくする
     dpi = 300)
+
+# ------------------------------------------------------------
+# 交絡変数を定義する
+age_labels <- c("16〜19歳", "20〜24歳", "25〜29歳", "30〜34歳", 
+                "35〜39歳", "40〜44歳", "45〜49歳", "50〜54歳", 
+                "55〜59歳", "60〜64歳", "65〜69歳", "70〜74歳", 
+                "75〜79歳", "80歳以上")
+analysis_data$gender <- factor(origin_data$F1, levels = 1:2, labels = c("男性", "女性"))
+analysis_data$age <- factor(origin_data$F2, levels = 1:14, labels = age_labels)
+
+# モデル1
+# 政府への信頼が「情報通信政策」の重要度認識に与える影響
+print("情報通信政策の係数プロット・交絡変数：性別")
+model_1 <- glm(is_important_q8_5 ~ trust_government + gender - 1, data = analysis_data, family = "binomial")
+summary(model_1)
+
+# モデル2
+# 政府への信頼が「科学技術イノベーション政策」の重要度認識に与える影響
+print("科学技術イノベーション政策の係数プロット・交絡変数：性別")
+model_2 <- glm(is_important_q8_19 ~ trust_government + gender - 1, data = analysis_data, family = "binomial")
+summary(model_2)
+
+# ------------------------------------------------------------
+
+# モデル1
+# 政府への信頼が「情報通信政策」の重要度認識に与える影響
+print("情報通信政策の係数プロット・交絡変数：年齢")
+model_1 <- glm(is_important_q8_5 ~ trust_government + age - 1, data = analysis_data, family = "binomial")
+summary(model_1)
+
+# モデル2
+# 政府への信頼が「科学技術イノベーション政策」の重要度認識に与える影響
+print("科学技術イノベーション政策の係数プロット・交絡変数：年齢を考慮した係数プロットを作成")
+model_2 <- glm(is_important_q8_19 ~ trust_government + age - 1, data = analysis_data, family = "binomial")
+summary(model_2)
+
